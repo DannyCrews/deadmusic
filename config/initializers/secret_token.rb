72db92c7c44fcc47e11cec780f156fc215c66757
Dan Crews <dancrews@me.com>
2013-12-15 16:52:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deadmusic::Application.config.secret_key_base = '0635c5b746322b6a6a8dec35e779032f8185424761f1b10c0db2337f9457d4565ab3e0bc2264c67512f925f85208d6d22c043c325094c9ebe209304da7ad6e71'
