50b74708464d52300fb017670682680ab595f0e3
Dan Crews <dancrews@me.com>
2013-12-15 20:45:43 -0500

    Added routes to api_controller. Added erb tag to index.html.erb. Added httparty gem.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,8 @@
 class ApiController < ApplicationController
+
+
+  def index
+    @setlistfm_data = http://api.setlist.fm/rest/0.1/artist/6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6/setlists.json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Deadmusic::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'api#index'
+
+  resources :api
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # get '/' => 'api#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
