60cc6269dc6f1a530bd07309bfd24b269af37f87
Dan Crews <dancrews@me.com>
2013-12-16 12:36:00 -0500

    read setlist data into app/assets/data/setlistfm.txt.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,17 @@
 class ApiController < ApplicationController
 
-
   def index
-    @setlistfm_data = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6/setlists.json')
+    i =1
+    @setlistfm_data = []
+    while i <= 115 do
+      @setlistfm_data << HTTParty.get("http://api.setlist.fm/rest/0.1/artist/6faa7ca7-0d99-4a5e-bfa6-1fd5037520c6/setlists.json?p=1")
+      # @setlistfm_data << page
+      i += 1
+    end
+
+    File.open("app/assets/data/setlistfm.txt", "w") do |file|
+    file.puts @setlistfm_data
+    end
   end
 
 end
